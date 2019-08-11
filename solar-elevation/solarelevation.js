8bb4808c1b2161dd154fd08b6f06dff713bdddc3
Mark Hartel <mark_hartel@hotmail.com>
2019-08-10 21:45:34 -0700

    add animation

--- solar-elevation/solarelevation.js
+++ solar-elevation/solarelevation.js
@@ -9,26 +9,31 @@ function radiansToDegrees(radians) {
 
 // This algoritm is from the book "The World is Round" by Tony Rothman.
 function SolarElevation(dayLength, yearLength, planetTilt, observerLatitude) {
-		this.dayLength = dayLength;
-		this.yearLength = yearLength;
-		this.planetTilt = planetTilt;
-		this.observerLatitude = observerLatitude;
-		this.theta = degreesToRadians(this.observerLatitude);
-		this.phi = degreesToRadians(this.planetTilt);
-		this.alpha = 2.0*Math.PI/this.dayLength;
-		this.beta = 2.0*Math.PI/this.yearLength;
-		this.costheta = Math.cos(this.theta);
-		this.sintheta = Math.sin(this.theta);
-		this.cosphi = Math.cos(this.phi);
-		this.sinphi = Math.sin(this.phi);
-    this.calculate = function(time) {
-		  var alphaAngle = this.alpha * time;
-		  var betaAngle = this.beta * time;
-		  var coszeta = ((this.costheta*this.cosphi*Math.cos(alphaAngle) + this.sintheta*this.sinphi)*Math.cos(betaAngle)) + 
-			  (this.costheta*Math.sin(alphaAngle)*Math.sin(betaAngle));
-		  var zeta = Math.acos(coszeta);
-		  var elevation = 90.0 - radiansToDegrees(zeta);
-		  return elevation;
-    };
+	this.dayLength = dayLength;
+	this.yearLength = yearLength;
+	this.planetTilt = planetTilt;
+	this.observerLatitude = observerLatitude;
+	this.theta = degreesToRadians(this.observerLatitude);
+	this.phi = degreesToRadians(this.planetTilt);
+	this.alpha = 2.0*Math.PI/this.dayLength;
+	this.beta = 2.0*Math.PI/this.yearLength;
+	this.costheta = Math.cos(this.theta);
+	this.sintheta = Math.sin(this.theta);
+	this.cosphi = Math.cos(this.phi);
+	this.sinphi = Math.sin(this.phi);
+	this.calculate = function(time) {
+		var alphaAngle = this.alpha * time;
+		var betaAngle = this.beta * time;
+		var coszeta = ((this.costheta*this.cosphi*Math.cos(alphaAngle) + this.sintheta*this.sinphi)*Math.cos(betaAngle)) + 
+			(this.costheta*Math.sin(alphaAngle)*Math.sin(betaAngle));
+		var zeta = Math.acos(coszeta);
+		var elevation = 90.0 - radiansToDegrees(zeta);
+		return elevation;
+	};
+	this.calculateAz = function(time) {
+		var x = time/this.dayLength;
+		var azimuth = (x - Math.floor(x)) * 360.0; 
+		return azimuth;
+	};
 }
 
