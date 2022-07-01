dfb5054e2ac199cde7ba720babf797b133abc96a
markhartel <mark_hartel@hotmail.com>
2022-06-30 17:01:28 -0700

    Update solarelevation.js

--- solar-elevation/solarelevation.js
+++ solar-elevation/solarelevation.js
@@ -27,16 +27,19 @@ function SolarElevation(dayLength, yearLength, planetTilt, observerLatitude) {
 		var coszeta = ((this.costheta*this.cosphi*Math.cos(alphaAngle) + this.sintheta*this.sinphi)*Math.cos(betaAngle)) + 
 			(this.costheta*Math.sin(alphaAngle)*Math.sin(betaAngle));
 		var zeta = Math.acos(coszeta);
-		var elevation = 90.0 - radiansToDegrees(zeta);
+		var elevation = -(90.0 - radiansToDegrees(zeta));
 		return elevation;
 	};
 	this.calculateAz = function(time) {
 		var alphaAngle = this.alpha * time;
 		var betaAngle = this.beta * time;
-		return radiansToDegrees(alphaAngle - betaAngle);
+		return radiansToDegrees(alphaAngle - betaAngle)  % 360;
 //		var x = time/this.dayLength;
 //		var azimuth = (x - Math.floor(x)) * 360.0; 
 //		return azimuth;
+	};
+	this.position = function(time) {
+		return [this.calculateAz(time), this.calculate(time)];
 	};
 }
 
